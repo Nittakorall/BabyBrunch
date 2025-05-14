7e8ee55dacbb2234cc3ab55a2553bf6cead66abf
Kseniia <anillinnittakorall@gmail.com>
2025-05-14 13:46:09 +0200

    init

---
+++ BabyBrunch/BabyBrunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyBrunchApp.swift
+//  BabyBrunch
+//
+//  Created by Kseniia on 2025-05-14.
+//
+
+import SwiftUI
+
+@main
+struct BabyBrunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
