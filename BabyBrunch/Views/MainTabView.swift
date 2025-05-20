84a719f7e3dd08ba1e8fb438e738719c9dd462be
Kseniia <veienosskog@gmail.com>
2025-05-20 11:50:43 +0200

    fixed tabview appearance, again

--- BabyBrunch/Views/MainTabView.swift
+++ BabyBrunch/Views/MainTabView.swift
@@ -8,30 +8,43 @@
 import SwiftUI
 
 struct MainTabView: View {
-   
-
-
+    //shows which tab shoud opens by default, uses tags
+    @State private var selectedTab = 2
+    
+    //Cince swiftUI doesn't let to change the color of inactive tab, added UIKit that was supposed to be in appDelegate
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.stackedLayoutAppearance.normal.iconColor = UIColor(named: "raisinBlack")
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(named: "oldRose")
+        UITabBar.appearance().standardAppearance = appearance
+        //add rows below if you want to add background to tabView
+        
+      //  if #available(iOS 15.0, *) {
+         //   UITabBar.appearance().scrollEdgeAppearance = appearance
+        //}
+    }
                 var body: some View {
-                    TabView {
+                    TabView(selection: $selectedTab) {
                     ProfileView()
                             .tabItem {
-                            Label("Profile", systemImage: "person.crop.circle.fill")
+                                Image(systemName: "person.crop.circle.fill")
                         }
+                            .tag(1)
                     
                         MapView()
                             .tabItem {
-                                Label("Map", systemImage: "map.fill")
+                                Image(systemName: "map.fill")
                             }
-                           // .badge("!")
+                            .tag(2)
 
                         FavouritesView()
                             .tabItem {
-                                Label("Favourites", systemImage: "heart.fill")
+                                Image(systemName: "heart")
                             }
+                            .tag(3)
                     }
                 }
             }
     #Preview {
         MainTabView()
     }
-
