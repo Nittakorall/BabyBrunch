b746306ddec1bec64b2a67707405f6e5d36477ed
KiwiComp <kim.bellander@gmail.com>
2025-05-26 16:04:39 +0200

    Created ReviewData model to easily hold both review text and rating. Added a list of ReviewData objects to pin model. Created function i MapViewModel to save review to pin on Firestore. When calling function in AddReviewVIew, added check to call only if user has written anything in the text editor.

---
+++ BabyBrunch/Models/ReviewData.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewData.swift
+//  BabyBrunch
+//
+//  Created by KiwiComp on 2025-05-26.
+//
+
+import Foundation
+
+struct ReviewData : Codable {
+   var text : String
+   var rating : Int
+}
