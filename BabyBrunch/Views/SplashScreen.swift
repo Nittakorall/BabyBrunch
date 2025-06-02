d0f36433e2ee9375ac15895a4c8201fd1ce7ec06
Kseniia <veienosskog@gmail.com>
2025-06-02 15:05:47 +0200

    added sound effect to splash screen

--- BabyBrunch/Views/SplashScreen.swift
+++ BabyBrunch/Views/SplashScreen.swift
@@ -6,11 +6,29 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct SplashScreen: View {
     @State private var isAnimating = false
     @Binding var isActive: Bool
     
+    var player: AVAudioPlayer? = nil
+
+    
+    //no idea why I had to add isActive: Binding<Bool> and   self._isActive = isActive, that was chatGPTs solution, don't ask
+    init(isActive: Binding<Bool>) {
+        self._isActive = isActive
+            if let soundURL = Bundle.main.url(forResource: "SplashScreenSound", withExtension: "wav") {
+                do {
+                    player = try AVAudioPlayer(contentsOf: soundURL)
+                    player?.prepareToPlay()
+                } catch {
+                    print("Couldn't play the sound")
+                }
+            } else {
+                print("Can't find the sound")
+            }
+        }
     
     var body: some View {
         ZStack {
@@ -28,7 +46,7 @@ struct SplashScreen: View {
         }
         .onAppear {
             isAnimating = true
-            
+            player?.play()
             // shows 2 minutes then proceed to another views
             DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                 withAnimation {
@@ -37,7 +55,9 @@ struct SplashScreen: View {
             }
         }
     }
+    
 }
+
 
 //#Preview {
 //    SplashScreen(isActive: true)
